9dedfc7a38144863952f7d347174ff6883b8c1b1
Syeda Sana Zehra <sana_sz91@hotmail.com>
2018-07-29 09:43:26 +0500

    valet info screen

--- lib/book_valet/book_valet_screen.dart
+++ lib/book_valet/book_valet_screen.dart
@@ -18,7 +18,7 @@ class BookValetState extends State<StatefulWidget> {
   @override
   void initState() {
     super.initState();
-    docReference = documentReference.document("valets/imran");
+    docReference = documentReference.document("imran");
   }
 
   @override
@@ -27,10 +27,12 @@ class BookValetState extends State<StatefulWidget> {
         appBar: AppBar(
           title: Text("Find a valet"),
         ),
+        bottomNavigationBar: getBottomAppBar(context),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         body: StreamBuilder(
             stream: docReference.snapshots(),
             builder: (context, snapshots) {
-             // if (!snapshots.hasData) return const Text('Loading...');
+              //if (!snapshots.hasData) return const Text('Loading...');
               return Center(
                 child: Container(
                   decoration: BoxDecoration(
@@ -43,37 +45,97 @@ class BookValetState extends State<StatefulWidget> {
                     child: Column(
                       children: <Widget>[
                         CircleAvatar(
-                          backgroundImage: AssetImage("assets/images/imran.jpeg"),
+                          backgroundImage:
+                              AssetImage("assets/images/imran.jpeg"),
                           radius: 60.0,
                         ),
+                        //       Expanded(
+                        // child:
                         Container(
+//                            height: 10.0,
+//                            width: 20.0,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: Colors.white,
+                            // border: Border.all(color: Colors.black,width: 1.0)
+                          ),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: <Widget>[
                               Text(
-                                "Name: Imran",
+                                "Name: imran",
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black,
+                                ),
                               ),
-                              Text("Id: 0123456789")
+                              Text(
+                                "Id: 0123456789",
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w500),
+                              ),
                             ],
                           ),
                         ),
-                        Center(
-                          child: ButtonBar(
-                            mainAxisSize: MainAxisSize.max,
-                            alignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              IconButton(
-                                  icon: Icon(Icons.call),iconSize: 40.0, onPressed: () => {}),
-                              IconButton(
-                                  icon: Icon(Icons.message),iconSize: 40.0, onPressed: () => {}),
-                            ],
+                        //   ),
+                        Expanded(
+                          child: Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Text(
+                                  "ETA: 3 minutes",
+                                  style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.green),
+                                ),
+                              ],
+                            ),
                           ),
-                        )
+                        ),
+
+//                        Center(
+//                          child: ButtonBar(
+//                            mainAxisSize: MainAxisSize.max,
+//                            alignment: MainAxisAlignment.center,
+//                            children: <Widget>[
+//                              IconButton(
+//                                  icon: Icon(Icons.call),iconSize: 40.0, onPressed: () => {}),
+//                              IconButton(
+//                                  icon: Icon(Icons.message),iconSize: 40.0, onPressed: () => {}),
+//                            ],
+//                          ),
+//                        )
                       ],
                     ),
                   ) /* add child content content here */,
                 ),
               );
-            }));
+            }),
+        floatingActionButton: FloatingActionButton.extended(
+          elevation: 4.0,
+          icon: const Icon(Icons.cancel),
+          label: new Text("Cancel"),
+        ));
+  }
+
+  BottomAppBar getBottomAppBar(BuildContext context) {
+    return BottomAppBar(
+      hasNotch: false,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.call),
+            onPressed: () => print("c"),
+          ),
+          IconButton(icon: Icon(Icons.message), onPressed: () => print("m")),
+        ],
+      ),
+    );
   }
 }
